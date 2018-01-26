23fdfbb0193ad8b55e2933b3ee4e8e39274bb409
Ruslan Kovtun <koutoftimer@gmail.com>
2018-01-26 10:57:08 +0200

    Move source code to src/ dir and add build script

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,22 @@ gulp.task('default', ['copy']);
 gulp.task('copy', function () {
 	return gulp
 		.src([
-			'./node_modules/pixi.js/dist/pixi.min.js',
-			'./node_modules/pixi-sound/dist/pixi-sound.js',
-			'./node_modules/hammerjs/hammer.min.js',
+			'./node_modules/pixi.js/dist/pixi.min.{js,js.map}',
+			'./node_modules/pixi-sound/dist/pixi-sound.{js,js.map}',
+			'./node_modules/hammerjs/hammer.min.{js,js.map}',
 			'./node_modules/mini-signals/browser.js',
 			'./node_modules/requirejs/require.js',
 		])
 		.pipe(gulp.dest('./src/js/lib/')); // pipe it to the output DIR
 });
 
-gulp.task('build', ['copy'], function () {
+gulp.task('build', function () {
 	return gulp
 		.src([
 			'./src/**/*.html',
 			'./src/main.js',
-			'./src/ogg/*',
-			'./src/**/*.js',
+			'./src/**/*.ogg',
+			'./src/**/**/*.{js,js.map}',
 		])
 		.pipe(gulp.dest('./build/'))
 });
